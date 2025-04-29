412c97379f9ecb21c958d8e7105d86d3391a755c
Rik Crompton <rik.crompton@howells-associates.com>
2025-04-29 08:52:53 +0100

    Add project files.

---
+++ KendoFormDatePicker/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace KendoFormDatePicker
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo/js").Include(
+                        "~/Scripts/kendo/2025.1.227/kendo.all.min.js",
+                        "~/Scripts/kendo/2025.1.227/kendo.aspnetmvc.min.js"
+                        ));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/kendo/2025.1.227/default-main.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ KendoFormDatePicker/Models/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KendoFormDatePicker.Models
+{
+    public class IndexViewModel
+    {
+        public DateTime DateOfBirth { get; set; } = new DateTime(1950, 04, 15);
+    }
+}
